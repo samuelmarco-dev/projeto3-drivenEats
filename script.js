85ef29a35d0cd6d642c5a064fa967f41c887824c
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-01-30 15:19:26 -0300

    tela de enviar pedido finalizada

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+let pratoSelecionado = null;
+let bebidaSelecionada = null;
+let sobremesaSelecionada = null;
+
+let meuPrato = null;
+let minhaBebida = null;
+let minhaSobremesa = null;
+
+let valorPrato = 0;
+let valorBebida = 0;
+let valorSobremesa = 0;
+
 /* Primeiro passo, limitar o click do usuário em apenas um tipo de prato, 
 seja prato principal, bebida ou sobremesa. No mais, quando o usuário clicar 
 em uma opção não selecionada, devemos adicionar a classe 'selecionado' */
 
-function selecionarOpcaoDePrato(pratoEscolhido, elementoIcone){
+function selecionarOpcaoDePrato(pratoEscolhido, elementoIcone, nomedeExibicao, precoPedido){
     console.log(pratoEscolhido, elementoIcone);
     const selecionado = document.querySelector('.tipo-de-prato .selecionado');
 
@@ -12,9 +24,17 @@ function selecionarOpcaoDePrato(pratoEscolhido, elementoIcone){
     }
     // Se a classe selecionado não está na tela, devemos adicionar
     pratoEscolhido.classList.add('selecionado');
+    
+    // Informações da minha escolha
+    meuPrato = pratoEscolhido;
+    pratoSelecionado = nomedeExibicao;
+    valorPrato = precoPedido;
+
+    // Verificar Botao
+    verificarItensSelecionado();
 }
 
-function selecionarOpcaoDeBebida(pratoEscolhido, elementoIcone){
+function selecionarOpcaoDeBebida(pratoEscolhido, elementoIcone, nomedeExibicao, precoPedido){
     console.log(pratoEscolhido, elementoIcone);
     const selecionado = document.querySelector('.tipo-de-bebida .selecionado');
 
@@ -24,9 +44,17 @@ function selecionarOpcaoDeBebida(pratoEscolhido, elementoIcone){
     }
     // Se a classe selecionado não está na tela, devemos adicionar
     pratoEscolhido.classList.add('selecionado');
+    
+    // Informações da minha escolha
+    minhaBebida = pratoEscolhido;
+    bebidaSelecionada = nomedeExibicao;
+    valorBebida = precoPedido;
+
+    // Verificar Botao
+    verificarItensSelecionado();
 }
 
-function selecionarOpcaoDeSobremesa(pratoEscolhido, elementoIcone){
+function selecionarOpcaoDeSobremesa(pratoEscolhido, elementoIcone, nomedeExibicao, precoPedido){
     console.log(pratoEscolhido, elementoIcone);
     const selecionado = document.querySelector('.tipo-de-sobremesa .selecionado');
 
@@ -36,7 +64,48 @@ function selecionarOpcaoDeSobremesa(pratoEscolhido, elementoIcone){
     }
     // Se a classe selecionado não está na tela, devemos adicionar
     pratoEscolhido.classList.add('selecionado');
+    
+    // Informações da minha escolha
+    minhaSobremesa = pratoEscolhido;
+    sobremesaSelecionada = nomedeExibicao;
+    valorSobremesa = precoPedido;
+
+    // Verificar Botao
+    verificarItensSelecionado();
 }
+
+/* Segundo passo, verificar se o usuário selecionou o combo DrivenEats, 
+que inclui um prato, uma bebida e uma sobremesa. A verificação será: se
+os três itens foram selecionados o botão na tag <footer> te´ra background
+green, caso contrário, devemos esperar até que os três itens sejam selecionados */
+
+function verificarItensSelecionado(){
+    let botaoDePedido = document.querySelector('footer .botao');
+    console.log(meuPrato, minhaBebida, minhaSobremesa, botaoDePedido);
+
+    if(meuPrato!==null && minhaBebida!==null && minhaSobremesa!==null){
+        botaoDePedido.style.backgroundColor = 'rgba(50, 183, 47, 1)';
+    }
+}
+
+/* Terceiro passo, após habilitar o background no botão, devemos mostrar ao 
+usuário os itens que ele selecionou como combo, a partir disto, devemos enca-
+minhá-lo para o WhatsApp Web do restaurante com uma mensagem pronta para ser
+enviada, contendo o valor em reais do combo escolhido*/
+
+let valorTotal = 0;
+
+function enviarPedido(){
+    let nomeUsuario = prompt('Por favor, nos informe seu nome:');
+    let enderecoUsuario = prompt('Agora, nos informe seu endereço:');
+    let bairroUsuario = prompt('Por último, nos informe seu bairro:');
+
+    valorTotal = (valorPrato + valorBebida + valorSobremesa).toFixed(2);
+    let mensagemDoPedido = window.encodeURIComponent(`Olá, gostaria de fazer o pedido:\n\n- Prato: ${pratoSelecionado}\n- Bebida: ${bebidaSelecionada}\n- Sobremesa: ${sobremesaSelecionada}\n\nTotal: R$ ${valorTotal}\n\nNome: ${nomeUsuario}\nEndereço: ${enderecoUsuario}\nBairro: ${bairroUsuario} - São Paulo`);
+    
+    window.location.href = `https://wa.me/5519994103269?text=${mensagemDoPedido}`;
+}
+
 
 // function selecionarTipoDePrato(elementoSelecionado, elementoFigure){
 //     console.log(elementoSelecionado);
