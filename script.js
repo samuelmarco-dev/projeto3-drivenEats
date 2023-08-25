d4098301f99d19eafb0b4575a460e783ae08da72
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2023-08-25 08:28:23 -0300

    feat: update

--- script.js
+++ script.js
@@ -14,57 +14,82 @@ let valorSobremesa = 0;
 seja prato principal, bebida ou sobremesa. No mais, quando o usuário clicar 
 em uma opção não selecionada, devemos adicionar a classe 'selecionado' */
 
-function selecionarOpcaoDePrato(pratoEscolhido, elementoIcone, nomedeExibicao, precoPedido){
-    console.log(pratoEscolhido, elementoIcone);
-    const selecionado = document.querySelector('.tipo-de-prato .selecionado');
-     
+function selecionarOpcaoDePrato(
+    pratoEscolhido,
+    elementoIcone,
+    nomedeExibicao,
+    precoPedido
+) {
+    const selecionado = document.querySelector(".tipo-de-prato .selecionado");
+    console.log(pratoEscolhido, selecionado, meuPrato);
+
     // Se a classe selecionado está na tela, devemos remover
-    if(selecionado !== null){
-        selecionado.classList.remove('selecionado');
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
     }
     // Se a classe selecionado não está na tela, devemos adicionar
-    pratoEscolhido.classList.add('selecionado');
-    
+    pratoEscolhido.classList.add("selecionado");
+
     // Informações da minha escolha
     meuPrato = pratoEscolhido;
     pratoSelecionado = nomedeExibicao;
     valorPrato = precoPedido;
 
+    if (pratoEscolhido == meuPrato) {
+        selecionado?.classList.remove("selecionado");
+    }
+
     // Verificar Botao
     verificarItensSelecionado();
 }
 
-function selecionarOpcaoDeBebida(pratoEscolhido, elementoIcone, nomedeExibicao, precoPedido){
-    console.log(pratoEscolhido, elementoIcone);
-    const selecionado = document.querySelector('.tipo-de-bebida .selecionado');
+function selecionarOpcaoDeBebida(
+    pratoEscolhido,
+    elementoIcone,
+    nomedeExibicao,
+    precoPedido
+) {
+    console.log("bebida", pratoEscolhido);
+    const selecionado = document.querySelector(".tipo-de-bebida .selecionado");
+    console.log(pratoEscolhido, selecionado, minhaBebida);
 
     // Se a classe selecionado está na tela, devemos remover
-    if(selecionado !== null){
-        selecionado.classList.remove('selecionado');
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
     }
     // Se a classe selecionado não está na tela, devemos adicionar
-    pratoEscolhido.classList.add('selecionado');
-    
+    pratoEscolhido.classList.add("selecionado");
+
     // Informações da minha escolha
     minhaBebida = pratoEscolhido;
     bebidaSelecionada = nomedeExibicao;
     valorBebida = precoPedido;
 
+    if (pratoEscolhido == minhaBebida) {
+        selecionado?.classList.remove("selecionado");
+    }
+
     // Verificar Botao
     verificarItensSelecionado();
 }
 
-function selecionarOpcaoDeSobremesa(pratoEscolhido, elementoIcone, nomedeExibicao, precoPedido){
-    console.log(pratoEscolhido, elementoIcone);
-    const selecionado = document.querySelector('.tipo-de-sobremesa .selecionado');
+function selecionarOpcaoDeSobremesa(
+    pratoEscolhido,
+    elementoIcone,
+    nomedeExibicao,
+    precoPedido
+) {
+    const selecionado = document.querySelector(
+        ".tipo-de-sobremesa .selecionado"
+    );
 
     // Se a classe selecionado está na tela, devemos remover
-    if(selecionado !== null){
-        selecionado.classList.remove('selecionado');
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
     }
     // Se a classe selecionado não está na tela, devemos adicionar
-    pratoEscolhido.classList.add('selecionado');
-    
+    pratoEscolhido.classList.add("selecionado");
+
     // Informações da minha escolha
     minhaSobremesa = pratoEscolhido;
     sobremesaSelecionada = nomedeExibicao;
@@ -79,15 +104,14 @@ que inclui um prato, uma bebida e uma sobremesa. A verificação será: se
 os três itens foram selecionados o botão na tag <footer> te´ra background
 green, caso contrário, devemos esperar até que os três itens sejam selecionados */
 
-function verificarItensSelecionado(){
-    let botaoDePedido = document.querySelector('footer .botao');
-    console.log(meuPrato, minhaBebida, minhaSobremesa, botaoDePedido);
+function verificarItensSelecionado() {
+    let botaoDePedido = document.querySelector("footer .botao");
 
-    if(meuPrato!==null && minhaBebida!==null && minhaSobremesa!==null){
-        botaoDePedido.innerHTML = 'Fechar pedido';
-        botaoDePedido.classList.add('texto-pedido');
-        botaoDePedido.style.fontWeight = '700';
-        botaoDePedido.style.backgroundColor = 'rgba(50, 183, 47, 1)';
+    if (meuPrato !== null && minhaBebida !== null && minhaSobremesa !== null) {
+        botaoDePedido.innerHTML = "Fechar pedido";
+        botaoDePedido.classList.add("texto-pedido");
+        botaoDePedido.style.fontWeight = "700";
+        botaoDePedido.style.backgroundColor = "rgba(50, 183, 47, 1)";
     }
 }
 
@@ -98,22 +122,24 @@ enviada, contendo o valor em reais do combo escolhido*/
 
 let valorTotal = 0;
 
-function enviarPedido(){
-    let nomeUsuario = prompt('Por favor, nos informe seu nome:');
-    let enderecoUsuario = prompt('Agora, nos informe seu endereço, número:');
-    let bairroUsuario = prompt('Por último, nos informe seu bairro:');
+function enviarPedido() {
+    let nomeUsuario = prompt("Por favor, nos informe seu nome:");
+    let enderecoUsuario = prompt("Agora, nos informe seu endereço, número:");
+    let bairroUsuario = prompt("Por último, nos informe seu bairro:");
 
     valorTotal = (valorPrato + valorBebida + valorSobremesa).toFixed(2);
-    let mensagemDoPedido = window.encodeURIComponent(`Olá, gostaria de fazer o pedido:\n\n- Prato: ${pratoSelecionado}\n- Bebida: ${bebidaSelecionada}\n- Sobremesa: ${sobremesaSelecionada}\n\nTotal: R$ ${valorTotal}\n\nNome: ${nomeUsuario}\nEndereço: ${enderecoUsuario}\nBairro: ${bairroUsuario} - São Paulo`);
-    
+    let mensagemDoPedido = window.encodeURIComponent(
+        `Olá, gostaria de fazer o pedido:\n\n- Prato: ${pratoSelecionado}\n- Bebida: ${bebidaSelecionada}\n- Sobremesa: ${sobremesaSelecionada}\n\nTotal: R$ ${valorTotal}\n\nNome: ${nomeUsuario}\nEndereço: ${enderecoUsuario}\nBairro: ${bairroUsuario} - São Paulo`
+    );
+
     window.location.href = `https://wa.me/5519994103269?text=${mensagemDoPedido}`;
 }
 
-function encaminharWhatsApp(){
-    alert('Caso esteja em dúvida sobre seu combo, continue aqui...');
-    const status = confirm('Gostaria de ser encaminhado para o WhatsApp?');
+function encaminharWhatsApp() {
+    alert("Caso esteja em dúvida sobre seu combo, continue aqui...");
+    const status = confirm("Gostaria de ser encaminhado para o WhatsApp?");
 
-    if(status){
+    if (status) {
         window.location.href = `https://wa.me/5519994103269`;
     }
 }
